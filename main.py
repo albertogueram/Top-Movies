6b550a8e681d0031a7f2cab3db3d9019bcc8cf2f
Alberto Guerrero <alberto11160@gmail.com>
2024-11-18 18:25:58 +0100

    Initial commit: Show the information from the movie table

---
+++ main.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import requests
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap5(app)
+
+# CREATE DB
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///movies.db"
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+
+# CREATE TABLE
+class Movie(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    year: Mapped[int] = mapped_column(Integer, nullable=False)
+    description: Mapped[str] = mapped_column(String(1000), nullable=False)
+    rating: Mapped[int] = mapped_column(Float, nullable=True)
+    ranking: Mapped[int] = mapped_column(Integer, nullable=True)
+    review: Mapped[str] = mapped_column(String(500), nullable=True)
+    img_url: Mapped[str] = mapped_column(String(500), nullable=False)
+
+    def __repr__(self):
+        return f'<Movie {self.title}>'
+
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route("/")
+def home():
+    result = db.session.execute(db.select(Movie).order_by(Movie.ranking))
+    all_movies = result.scalars().all()
+    return render_template("index.html", movies=all_movies)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
